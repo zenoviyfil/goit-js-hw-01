e29085db1af95d6bc06418c869147f80959025bd
Zenoviy Fil <fil.zenoviy@gmail.com>
2023-10-24 19:57:09 +0300

    task 2 complited

--- js/task-1.js
+++ js/task-1.js
@@ -5,6 +5,7 @@ function makeTransaction(quantity, pricePerDroid) {
       } credits!`
     );
 }
+
     console.log(makeTransaction(5, 3000));
     console.log(makeTransaction(3, 1000));
     console.log(makeTransaction(10, 500));
